83a96959e94750725e5dd085147d3109dc0a53bb
1x481n <1x481n@gmail.com>
2022-07-17 16:37:04 +0800

    feat: 引入pest组件；规范测试方法命名

--- demo/src/tests/Unit/BPMServiceTest.php
+++ demo/src/tests/Unit/BPMServiceTest.php
@@ -2,7 +2,13 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Domain\Generic\BPM\Application\Services\ProcessRunningService;
+use App\Domain\Generic\BPM\Examples\ConcreteHandler;
+use App\Models\Business\BusinessType;
+//use PHPUnit\Framework\TestCase;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Tests\TestCase;
 
 class BPMServiceTest extends TestCase
 {
@@ -14,5 +20,30 @@ class BPMServiceTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * @return void
+     * @throws Exception|GuzzleException
+     */
+    public function test_start_process()
+    {
+        $result = app(ProcessRunningService::class)->startProcess(
+            BusinessType::find(1),
+            app(ConcreteHandler::class),
+            1,
+            1,
+            '111111111111',
+            11,
+            11,
+            [
+                //'apply_id'=>1,
+                'apply_amount' => '6000.00'
+            ],
+            ['ext1' => '扩展1'],
+            'enjoy yourself!'
+        );
+
+        $this->assertArrayHasKey('bpm_transaction_sn', $result,'发起流程测试用例运行失败！');
     }
 }
